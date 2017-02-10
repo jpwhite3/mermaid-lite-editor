dea662d406c85205a0f55e2bc0cf028a3a07d64a
Your Name <email@you.com>
2017-02-10 03:14:47 -0500

    first

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name LagoonApp
+ * @description
+ * # LagoonApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('LagoonApp', ['ngSanitize', 'ab-base64'])
+  .config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{%');
+    $interpolateProvider.endSymbol('%}');
+  })
+  .config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|local|data):/);
+  }]);
